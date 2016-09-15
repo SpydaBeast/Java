649321cd5469ba149c18aeec42c515511f8e7bfe
SpydaBeast <brien@artofmars.org>
2016-09-15 17:15:54 -0400

    Practicing Java

---
+++ Encapsulation/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Encapsulation Lesson");
+
+        //Encapsulation access to individual functions can be restricted
+
+        
+
+    }
+}
